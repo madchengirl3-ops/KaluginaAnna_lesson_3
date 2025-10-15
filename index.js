bd7477b1bde2f51211dfa23aa78c8eca0868f654
Anna Kalugina <madchengirl3@gmail.com>
2025-10-15 18:56:05 +0300

    дни недели

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
-const age = prompt("Введите ваш возраст:");
-let result = age < 18 ? alert ("Доступ запрещен!") : alert ("Доступ разрешен!");
+const day = prompt("Введи день недели:");
+
+switch(day) {
+    case"1" :
+    alert("Понедельник");
+    break;
+    case"2" :
+    alert("Вторник");
+    break;
+    case"3" :
+    alert("Среда");
+    break;
+    case"4" :
+    alert("Четверг");
+    break;
+    case"5" :
+    alert("Пятница");
+    break;
+    case"6" :
+    alert("Суббота");
+    break;
+    case"7" :
+    alert("Воскресенье");
+    break;
+    default :
+    alert("Ошибка: такого дня нет");
+    break;
+}
