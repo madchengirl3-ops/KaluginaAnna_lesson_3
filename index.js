e8dc5038aa7335b3b109b2133d43e4942121b24f
Anna Kalugina <madchengirl3@gmail.com>
2025-10-17 21:45:38 +0300

    год полета

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-let day = 0;
-let n = 50;
-let m = 7;
+let firstFlight = 1961;
+let currentYear = 2025;
+let found = false;
 
-while (n >= m) {
-    n /= 2;
-    day++;
+for (let year = currentYear; year >= firstFlight; year--) {
+    if (year === firstFlight) {
+        console.log(`Год первого полета человека в космос: ${year}`);
+        found = true;
+        break;
+        
+    }
 }
-console.log(`На ${day}-й день количество машин стало меньше ${m}`);
 
 
