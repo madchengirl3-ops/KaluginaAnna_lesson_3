dcd9efd7b2beeefcabbf6a632b6416f333016e09
Anna Kalugina <madchengirl3@gmail.com>
2025-10-17 21:30:27 +0300

    количество машин

--- index.js
+++ index.js
@@ -1,18 +1,11 @@
-let role = 'guest';
+let day = 0;
+let n = 50;
+let m = 7;
 
-switch (role) {
-    case 'admin':
-        console.log('Добро пожаловать, администратор!');
-        break;
-    case 'manager':
-        console.log('Привет, менеджер!');
-        break;
-    case 'user':
-        console.log('Рады видеть вас снова!');
-        break;
-    case 'guest':
-        console.log('Пожалуйста, войдите в систему!')
-        break;
-        
+while (n >= m) {
+    n /= 2;
+    day++;
 }
+console.log(`На ${day}-й день количество машин стало меньше ${m}`);
+
 
